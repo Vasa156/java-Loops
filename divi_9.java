547e3f7a8d673a574e6aaddc3e86a0f6c8ee69b5
Vasanthi Selvam <practicesmakesperfect0.1@gmail.com>
2024-10-13 20:12:03 +0530

    divi_9.java

---
+++ divi_9.java
@@ -0,0 +1,19 @@
+//assign a number 100-200 ,and divisble by 9 
+class divi_9
+ {
+     public static void main(String k[])
+      {
+          System.out.println("Enter the Number 100 to 200 and div by 9 : ");
+          int sum=0;
+          for(int i=101;i<200;i++)
+           {
+               if(i%9==0)
+                {
+                    System.out.println(i);
+                    
+                }
+                
+           }
+           
+      } 
+ }
